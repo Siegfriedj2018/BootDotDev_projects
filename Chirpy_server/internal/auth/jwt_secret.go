54e3215ea98b1d6a485e56edc9dffa56f6b412e9
Siegfriedj2018 <siegfrij@email.sc.edu>
2025-11-01 18:00:40 -0400

    fix Todo

--- Chirpy_server/internal/auth/jwt_secret.go
+++ Chirpy_server/internal/auth/jwt_secret.go
@@ -1,14 +1,12 @@
 package auth
 
 import (
-	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
-	"iter"
-	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // Note to future self openfile does not understand ~
@@ -32,46 +30,26 @@ func CreateJWTSecret() (int, error) {
 
 	fullPath := filepath.Join(homeDir, "my_bootdotdev_projects/BootDotDev_projects/Chirpy_server/.env")
 
-	file, err := os.OpenFile(fullPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		return 0, fmt.Errorf("file failed to open: %w", err)
-	}
+	// file, err := os.OpenFile(fullPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	// if err != nil {
+	// 	return 0, fmt.Errorf("file failed to open: %w", err)
+	// }
 
-	defer file.Close()
-	readBytes := []byte{}
-	_, err = file.Read(readBytes)
+	// defer file.Close()
+	file, err := os.ReadFile(fullPath)
 	if err != nil {
 		return 0, err
 	}
 
-	var val1, val2 []byte
-	var ok1 bool
-	var ok2 bool
-	line := bytes.Lines(readBytes)
-	nextVal, stop := iter.Pull(line)
-	value, _ := nextVal()
-	log.Printf("Lines: %v\n", value)
-	defer stop()
-	for {
-		val1, ok1 = nextVal()
-		if !ok1 {
-			stop()
-			log.Printf("don't know what i am doing here: %v", val1)
-			break
-		}
-		val2, ok2 = nextVal()
-		if !ok2 {
-			stop()
-			log.Printf("Dont know where to go from here: %v", val2)
-			break
-		}
+	convertedBytes := string(file)
+	replaceString := jwtString + "\"" + keyString + "\""
 
-		log.Printf("Val1: %v, Ok: %v", val1, ok1)
-		log.Printf("Val2: %v, Ok: %v", val2, ok2)
+	// TODO: Fix the bug where it find jwtstring and just appends the key on previous key instead of replacing it
+	jwtSecret := strings.ReplaceAll(convertedBytes, jwtString, replaceString)
+	err = os.WriteFile(fullPath, []byte(jwtSecret), 0644)
+	if err != nil {
+		return 0, err
 	}
-	log.Printf("Val1: %v, Ok: %v", val1, ok1)
-	log.Printf("Val2: %v, Ok: %v", val2, ok2)
 
-	jwtSecret := bytes.ReplaceAll(readBytes, val2, []byte("\n" + jwtString+"\""+keyString+"\""))
-	return file.Write(jwtSecret)
+	return len(convertedBytes), nil
 }
